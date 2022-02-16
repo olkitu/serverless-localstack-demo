b6437b58aba8b3ab73245d86ea99364c920603d4
Minh Truong <minh@truong.fi>
2022-02-16 21:43:12 +0200

    First Commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Serverless Lcoal Stack Demo
+
+This is just my own study / demo version.
+
+## Prerequisites
+
+* Install Docker
+* Install LocalStack: https://docs.localstack.cloud/get-started/
+
+# Start
+
+```
+localstack start
+npm ci
+serverless deploy --stage local
+```
---
+++ handler.js
@@ -0,0 +1,18 @@
+"use strict";
+
+module.exports.hello = async (event) => {
+  return {
+    statusCode: 200,
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(
+      {
+        message: "Go Serverless v2.0! Your function executed successfully!",
+        input: event,
+      },
+      null,
+      2
+    ),
+  };
+};
